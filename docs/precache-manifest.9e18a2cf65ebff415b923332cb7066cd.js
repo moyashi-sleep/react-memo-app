749c1b7a3a8648803b44dc1e30693a9a86b185ae
moyashi-sleep <53850092+moyashi-sleep@users.noreply.github.com>
2019-12-24 21:13:44 +0900

    [add]Pages公開用のdocsディレクトリ追加
    Pagesに公開するためのdocsディレクトリを作成しました。

---
+++ docs/precache-manifest.9e18a2cf65ebff415b923332cb7066cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8e7af83cdad014813b15b2a029777c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239d9fdabb35904b3d28",
+    "url": "/static/js/2.1b57277d.chunk.js"
+  },
+  {
+    "revision": "0654e6a34f81c0d0bd2a",
+    "url": "/static/js/main.47f08834.chunk.js"
+  },
+  {
+    "revision": "4512f8d028d6bd7495b8",
+    "url": "/static/js/runtime-main.2e104ebc.js"
+  }
+]);
